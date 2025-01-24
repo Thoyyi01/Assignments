d3fcd52644a274e622e9adec0be4af85dbaaf60c
Thoyyi01 <164750412+Thoyyi01@users.noreply.github.com>
2025-01-24 12:17:39 +0530

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,16 @@
+
+def calculate_square(n):
+    return n*n
+k=int(input())
+print(calculate_square(k))
+
+-----------
+a=int(input())
+for i in range(1,a):
+    print(i)
+i=0
+t=0
+while(i>=n):
+    t+=i
+    i+=1
+print(t)
